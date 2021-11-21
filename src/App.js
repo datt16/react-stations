aed0ee1da37cef63e91bd9b4d185fe9334f4e53b
datt16 <osatom.4639@gmail.com>
2021-11-22 08:22:18 +0900

    :+1: station3

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
     <div>
-      <header>This is Header</header>
+      <header>
+        <h1>Dogアプリ</h1>
+      </header>
+      <p>犬の画像を表示するサイトです。</p>
+      <img
+        src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"
+        alt="犬の画像"
+      />
     </div>
   )
 }
